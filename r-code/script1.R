7dc6dbd6f2e900b28acb61c7629b3b105b4ba6de
rodrigo zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2017-02-20 10:26:12 -0300

    Gitignore updated and first R script updated also.
    It is not important. Just simple improvements

--- r-code/script1.R
+++ r-code/script1.R
@@ -3,5 +3,8 @@ setwd('~/zenlabs/GEFCom2012_3/r-code')
 
 mydata = read.csv("../datasets/Load_history.csv")  # read csv file 
 
+zone_1 = subset(mydata, zone_id==1)
+
+typeof(mydata)
 x <- "hellow"
 print(x)
